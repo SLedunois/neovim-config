2f0f656917c0e586662baa8ae81233fadd0e342f
LEDUNOIS Simon <simon.ledunois@gmail.com>
2024-05-27 10:03:34 +0000

    chore: add ensure installation

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -12,6 +12,11 @@ return {
     opts = {
       auto_install = true,
     },
+    config = function ()
+      require("mason-lspconfig").setup({
+        ensure_installed = { "lua_ls", "lua", "tsserver", "ansiblels", "java_language_server" }
+      })
+    end
   },
   {
     "neovim/nvim-lspconfig",
