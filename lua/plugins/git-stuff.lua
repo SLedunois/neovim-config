fa18fa29a7b5dd01d387cbf29f4ca1fd05aaff50
Simon Ledunois <simon.ledunois@cgi.com>
2024-06-01 14:57:30 +0200

    Chore: adds which key

--- lua/plugins/git-stuff.lua
+++ lua/plugins/git-stuff.lua
@@ -10,5 +10,5 @@ return {
       vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<CR>", {})
       vim.keymap.set("n", "<leader>gt", ":Gitsigns toggle_current_line_blame<CR>", {})
     end
-  }
+  },
 }
